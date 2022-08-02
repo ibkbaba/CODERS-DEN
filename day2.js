5de701060d76656b9a8651f44b47e1ea9cb4da29
ibkbaba <faladeibukun2003@gmail.com>
2022-08-02 18:26:17 +0100

    day 2

---
+++ day2.js
@@ -0,0 +1,41 @@
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(2))
+console.log(challenge.substring(2,0))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+let social = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(social.split(','))
+console.log(challenge.replace('JavaScript','python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+let string = 'You cannot end a sentence with because because because is a conjunction'
+console.log(string.lastIndexOf('because'))
+console.log(string.indexOf('because'))
+let trim = ' 30 Days Of JavaScript '
+console.log(trim.trim())
+console.log(challenge.startsWith('3'))
+console.log(challenge.endsWith('t'))
+console.log(challenge.match('a'))
+let jav = '30 Days of'
+let scrip = ' JavaScript'
+console.log(jav.concat(scrip))
+console.log(challenge.repeat(2))
+
+
+
+
+
+
+
+
+
+
+
+
