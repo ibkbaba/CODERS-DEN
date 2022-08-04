d71cfd76f0b412c0a931a1c57ed8d9bbd26005d2
ibkbaba <faladeibukun2003@gmail.com>
2022-08-04 10:11:39 +0100

    day 1

---
+++ day1.js
@@ -0,0 +1,35 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable, easy to reuse and informative
+*/
+variable1 = 'value' 
+variable2= 40;
+variable3 = true
+variable4 = null
+variable5 = undefined
+console.log(typeof(variable1))
+console.log(typeof(variable2))
+console.log(typeof(variable3))
+console.log(typeof(variable4))
+console.log(typeof(variable5))
+let var1;
+let var2;
+let var3;
+let var4;
+
+let value1 = true;
+let value2 = 'java'
+let value3 = 'python'
+let value4 = 'c++'
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstname = 'falade'
+let lastname = 'ibukun'
+let maritalstatus = 'single'
+let country = 'Nigeria'
+let age = 19
+let Firstname = 'falade', Lastname = 'ibukun', Maritalstatus ='single', Country = 'Nigeria', Age = 19;
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 25;
+let yourAge = 30;
+console.log(`if i am ${myAge}.\n you are ${yourAge}.`)
